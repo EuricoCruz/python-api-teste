1d1b0ce92645d624c4fb1897723616cbc58a78ab
EuricoCruz <euricoscruz@hotmail.com>
2019-10-21 13:54:12 -0300

    add product model

--- server-side/app.py
+++ server-side/app.py
@@ -2,17 +2,22 @@ from flask import Flask, request, flash
 import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-
 mydb = myclient["TangoTest"]
+products = mydb["products"]
 
 app = Flask(__name__)
 
 print(myclient.list_database_names())
-
+print(mydb.list_collection_names())
 
 @app.route('/')
 def index():
-    return "<h1>Teste</h1>"
+  mydict = { "name": "Peter", "address": "Lowstreet 27" }
+
+  product = products.insert_one(mydict)
+
+  print(product.inserted_id)
+  return "<h1>Teste</h1>"
 
 @app.route('/produtos')
 def list_products():
