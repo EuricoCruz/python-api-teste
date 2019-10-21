096a3ed865f62b61931b867bf0db5d17f8338566
EuricoCruz <euricoscruz@hotmail.com>
2019-10-21 20:16:16 -0300

    server response with json

--- server-side/app.py
+++ server-side/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, request, flash
+from flask import Flask, request, flash, redirect, url_for
+from model import Product
 import pymongo
+import json
+from bson import json_util
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["TangoTest"]
@@ -7,21 +10,18 @@ products = mydb["products"]
 
 app = Flask(__name__)
 
-print(myclient.list_database_names())
-print(mydb.list_collection_names())
-
 @app.route('/')
 def index():
-  mydict = { "name": "Peter", "address": "Lowstreet 27" }
-
-  product = products.insert_one(mydict)
-
-  print(product.inserted_id)
   return "<h1>Teste</h1>"
 
+items = []
 @app.route('/produtos')
 def list_products():
-    return "<h1>Teste</h1>"
+    for product in products.find():
+      items.append(product)
+
+    print(items)
+    return json.dumps(items, default=str)
 
 @app.route('/produtos/:id')
 def list_one_product():
@@ -29,7 +29,16 @@ def list_one_product():
 
 @app.route('/produtos', methods=['POST',])
 def add_product():
-    return "<h1>Teste</h1>"
+    nome = request.form['nome']
+    quantidade = request.form['quantidade']
+    preco = request.form['preco']
+    codigo = request.form['codigo']
+    categoria = request.form['categoria']
+
+    item = Product(nome, quantidade, preco, codigo, categoria)
+    item = item.__dict__  
+    products.insert_one(item)
+    return json.dumps(item, default=str)
 
 @app.route('/produtos/:id')
 def edit_product():
---
+++ server-side/model.py
@@ -0,0 +1,11 @@
+class Product(object):
+  def __init__(self, nome, quantidade, preco, codigo, categoria):
+    self._nome = nome
+    self._quantidade = quantidade
+    self._preco      = preco
+    self._codigo    = codigo
+    self._categoria  = categoria
+
+
+  
+  
